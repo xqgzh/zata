7e6366a44a71a111d64638ba9454e0f5061f97ad
xqgzh <giester@gmail.com>
2012-02-02 15:55:46 +0800

    实现了HttpCacheAction

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,13 +12,51 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Test1.TestPerf();
+
+            TY t = new TY();
+
+            TY Current = t;
+
+            for (int i = 1; i < 10; i++)
+            {
+                TY x = new TY(){Number = i};
+
+                Current.Inner = x;
+
+                Current = x;
+            }
+
+            foreach (var y in For(t))
+            {
+                Console.WriteLine(y.Number);
+            }
+            
         }
 
-        
+        static List<TY> For(TY t)
+        {
+            List<TY> list = new List<TY>();
+
+            TY Current = t;
+
+            while (Current != null)
+            {
+                list.Add(Current);
+
+                Current = Current.Inner;
+            }
+
+            return list;
+        }
 
     }
 
+    public class TY
+    {
+        public int Number = 0;
+
+        public TY Inner;
+    }
 }
 
 
